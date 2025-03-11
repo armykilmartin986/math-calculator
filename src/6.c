69fdd43f5bf671bed6394650161d516f51484637
armykilmartin986 <armykilmartin986@gmail.com>
2025-03-11 16:05:11 +0000

    Add 6.c

---
+++ src/6.c
@@ -0,0 +1,7 @@
+int main() {
+    float num1 = 5.0;
+    float num2 = 7.5;
+    float result = (num1 + num2) / 2.0;
+    printf("The average of %f and %f is %f\n", num1, num2, result);
+    return 0;
+}
