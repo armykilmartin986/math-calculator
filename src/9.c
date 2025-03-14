a7c120b4f012f444e066a446c29dd2ee72fed717
armykilmartin986 <armykilmartin986@gmail.com>
2025-03-14 10:08:47 +0000

    Add 9.c

---
+++ src/9.c
@@ -0,0 +1,11 @@
+  #include<stdio.h>
+ int main(){
+	//get user input
+	float x;
+	printf("enter your number:\n");
+	scanf("%f",&x);
+	
+	//perform operation
+	float result = x + 5;
+	printf("result is: %f\n",result);
+}
