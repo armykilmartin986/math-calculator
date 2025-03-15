e0c7790598af4ba37df9b67dec32d78cad146159
armykilmartin986 <armykilmartin986@gmail.com>
2025-03-15 01:29:12 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,10 @@
+
+int main() {
+    float num1 = 5;
+    float num2 = 7;
+    float result = (num1 + num2) / 2;
+    
+    printf("The average of %0.2f and %0.2f is %0.2f\n", num1, num2, result);
+    
+    return 0;
+}
