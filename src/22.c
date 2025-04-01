928a9a301005424a2fc46ad48669b035d8de34a2
armykilmartin986 <armykilmartin986@gmail.com>
2025-04-01 15:21:52 +0000

    Add 22.c

---
+++ src/22.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("This is a simple math-calculator.\n");
+    printf("You can add, subtract, multiply, and divide numbers.\n");
+    printf("Example: 5 + 3 = 8\n");
+    return 0;
+}
