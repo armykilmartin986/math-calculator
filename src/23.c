b7d88d7d9ab49567105deb2e5000946c55bd7329
armykilmartin986 <armykilmartin986@gmail.com>
2025-04-02 00:01:45 +0000

    Add 23.c

---
+++ src/23.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 5;
+    int b = 3;
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %.2f\n", a / b);
+    return 0;
+}
