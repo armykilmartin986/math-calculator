eac6319d9dd7d2f00d242ce3fce741617959a1b6
armykilmartin986 <armykilmartin986@gmail.com>
2025-04-06 07:22:59 +0000

    Add 26.c

---
+++ src/26.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int num1, num2;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("Addition of %d and %d is: %d\n", num1, num2, num1 + num2);
+    return 0;
+}
